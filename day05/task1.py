23c6f103b2470bbea1c4897160e2fde257c4a0e6
Matthew Castrillon-Madrigal <27matt37@gmail.com>
2020-05-19 13:47:17 +0100

    Day 05 Complete. Neccessary need for refactor

--- day05/task1.py
+++ day05/task1.py
@@ -3,8 +3,9 @@ program_string_array = program_file.read().strip('\n').split(',')
 
 program = [int(i) for i in program_string_array]
 
-test = [1, 0, 0, 0, 99]
+#program = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31, 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104, 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
 
+INPUT_ID = 5
 
 def apply_noun(noun):
     global program
@@ -16,7 +17,6 @@ def apply_verb(verb):
     program[2] = verb
 
 
-PARAMETER = 150
 
 def mode_support (program, position, mode):
     if (mode == 0): # POSITION MODE 
@@ -34,7 +34,7 @@ def multiply (parameter_1, parameter_2):
     return parameter_1 * parameter_2
 
 def store ( position):
-    program[position] = 1
+    program[position] = 5
 
 def read (position):
     global program
@@ -45,12 +45,14 @@ def run_program():
     print(program)
     index = 0
     while program[index] != 99:
-        opcode = program[index] % 10
+
+        opcode = int(str(program[index]).zfill(5)[-2:])
 
         if (opcode == 0):
             print('bad opcode: ', opcode)
             index += 1
             continue
+
 
         parameter_1_mode = int(str(program[index]).zfill(5)[-3])
         parameter_2_mode = int(str(program[index]).zfill(5)[-4])
@@ -77,9 +79,53 @@ def run_program():
             if (parameter_1 != 0):
                 break
             index += 2
+        elif (opcode == 5):
+            # JUMP IF TRUE
+            parameter_1 =  mode_support(program,program[index + 1], parameter_1_mode)
 
+            if (parameter_1 != 0):
+                parameter_2 =  mode_support(program,program[index + 2], parameter_2_mode)
+                index = parameter_2
+            else:
+                index += 3
+        elif (opcode == 6):
+            # JUMP IF TRUE
+            parameter_1 =  mode_support(program,program[index + 1], parameter_1_mode)
+
+
+            if (parameter_1 == 0):
+                parameter_2 =  mode_support(program,program[index + 2], parameter_2_mode)
+                index = parameter_2
+            else:
+                index += 3
+        elif (opcode == 7):
+
+            parameter_1 =  mode_support(program,program[index + 1], parameter_1_mode)
+            parameter_2 =  mode_support(program,program[index + 2], parameter_2_mode)
+
+
+            if (parameter_1 < parameter_2):
+                program[program[index+3]] = 1
+            else:
+                program[program[index+3]] = 0
+
+            index += 4
+        elif (opcode == 8):
+            parameter_1 =  mode_support(program,program[index + 1], parameter_1_mode)
+            parameter_2 =  mode_support(program,program[index + 2], parameter_2_mode)
+
+
+            if (parameter_1 == parameter_2):
+                program[program[index+3]] = 1
+            else:
+                program[program[index+3]] = 0
+
+            index += 4
+        elif (opcode == 99):
+            print('HALT: ', opcode)
+            break
         else:
             print('bad opcode: ', opcode)
             index += 1
 
-print(run_program())
+run_program()
